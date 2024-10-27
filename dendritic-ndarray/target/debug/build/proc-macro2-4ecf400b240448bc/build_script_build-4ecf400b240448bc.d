7419cad20a3a3cc7ea05c6da0ac6f5feff2e799e
shaysingh818 <shalinsingh818@gmail.com>
2024-10-27 13:34:28 -0500

    test:

--- dendritic-ndarray/target/debug/build/proc-macro2-4ecf400b240448bc/build_script_build-4ecf400b240448bc.d
+++
@@ -1,5 +0,0 @@
-/home/dread/Documents/Projects/steepgrad/ndarray/target/debug/build/proc-macro2-4ecf400b240448bc/build_script_build-4ecf400b240448bc: /home/dread/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.86/build.rs
-
-/home/dread/Documents/Projects/steepgrad/ndarray/target/debug/build/proc-macro2-4ecf400b240448bc/build_script_build-4ecf400b240448bc.d: /home/dread/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.86/build.rs
-
-/home/dread/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.86/build.rs:
