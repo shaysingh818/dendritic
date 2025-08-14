0a70592445b9a90ac180b2fc7bb4f039df236ef2
shaysingh818 <shalinsingh818.com>
2025-08-13 22:08:19 -0500

    add latest changes

--- crates/dendritic-preprocessing/src/preprocessing.rs
+++ crates/dendritic-preprocessing/src/preprocessing.rs
@@ -1,6 +1,6 @@
 use log::debug;
 use std::collections::HashSet; 
-use ndarray::{arr2, Array2, Axis};
+use ndarray::{arr2, Array, Array2, Axis};
 
 
 pub trait Preprocessor {
@@ -9,7 +9,7 @@ pub trait Preprocessor {
     fn encode(&mut self) -> Array2<f64>;
 
     /// Decode data from encoded values with associated trait type
-    fn decode(&mut self) -> Array2<f64>;
+    fn decode(&mut self, data: Array2<f64>) -> Array2<f64>;
 
 }
 
@@ -18,9 +18,6 @@ pub struct OneHotEncoding {
     
     /// Raw data passed to the encoder
     data: Array2<f64>,
-
-    /// Encoded data from the input
-    encoded: Array2<f64>,
 
     /// Max value associated with encoder
     num_classes: usize,
@@ -47,7 +44,6 @@ impl OneHotEncoding {
 
         Ok(Self {
             data: data.clone(),
-            encoded: Array2::zeros((data.dim().0, num_classes)),
             num_classes: num_classes, 
             num_samples: data.nrows()
         }) 
@@ -57,11 +53,6 @@ impl OneHotEncoding {
     /// Retrieve data passed to one hot encoder
     pub fn data(&self) -> Array2<f64> {
         self.data.clone()
-    }
-
-    /// Retrieve encoded data after it's been one hot encoded
-    pub fn encoded(&self) -> Array2<f64> {
-        self.encoded.clone()
     }
 
     /// Retrieve the number of classes associated with data
@@ -81,18 +72,111 @@ impl Preprocessor for OneHotEncoding {
 
     /// Encode function for One Hot Encoding
     fn encode(&mut self) -> Array2<f64> {
-        let mut row = 0; 
-        let col_stride = self.encoded.dim().1;
-        for row in self.data() {
-            row += col_stride;
+
+        let mut row: usize = 0;
+        let encoded_shape = (self.data.nrows(), self.num_classes);
+        let mut encoded: Array2<f64> = Array2::zeros(encoded_shape);
+        for (idx, row) in self.data().iter().enumerate() {
+            encoded[[idx, *row as usize]] = 1.0;
         }
-        self.data.clone()
+        encoded
     }
 
     /// Decode function for decoding One Hot Encoded Values
-    fn decode(&mut self) -> Array2<f64> {
-        println!("Not done yet"); 
+    fn decode(&mut self, data: Array2<f64>) -> Array2<f64> {
+
+        let mut decoded: Array2<f64> = Array2::zeros((data.nrows(), 1));
+        for (idx, row) in data.axis_iter(Axis(0)).enumerate() {
+            if let Some(col) = row.iter().position(|&x| x == 1.0) {
+                decoded[[idx, 0]] = col as f64;
+            }
+        }
+        decoded
+    }
+
+}
+
+
+#[derive(Debug, Clone)]
+pub struct StandardScalar {
+    
+    /// Raw data passed to the encoder
+    data: Array2<f64>,
+
+    /// Max value associated with encoder
+    mean: Vec<f64>,
+
+    /// Number of samples that were encoded
+    standard_deviation: Vec<f64>
+
+}
+
+
+impl StandardScalar {
+
+    pub fn new(data: &Array2<f64>) -> Result<Self, String> {
+         
+        Ok(Self {
+            data: data.clone(),
+            mean: vec![0.0; data.ncols()],
+            standard_deviation: vec![0.0; data.ncols()]
+        }) 
+
+    }
+
+    pub fn data(&self) -> Array2<f64> {
         self.data.clone()
+    }
+
+    pub fn mean(&self) -> Vec<f64> { 
+        self.mean.clone()
+    }
+
+    pub fn stdev(&self) -> Vec<f64> {
+        self.standard_deviation.clone()
+    }
+
+}
+
+
+impl Preprocessor for StandardScalar {
+
+    /// Encode function for One Hot Encoding
+    fn encode(&mut self) -> Array2<f64> {
+
+        let encoded: Array2<f64> = Array2::zeros(self.data.dim());
+     
+        for (idx, col) in self.data.axis_iter(Axis(1)).enumerate() {
+
+            self.mean[idx] = col.mean().unwrap();
+            let mean_vec = Array::from_elem(
+                self.data.nrows(), 
+                self.mean[idx]
+            );
+
+            let mut diffs = col.to_owned() - mean_vec.clone();
+            diffs.mapv_inplace(|x| x * x); 
+
+            let variance = diffs.sum() / self.data.nrows() as f64;
+            self.standard_deviation[idx] = variance.sqrt();
+            let std_dev_vec = Array::from_elem(
+                self.data.nrows(), 
+                self.standard_deviation[idx]
+            );
+
+            let feature_col = (col.to_owned() - mean_vec.clone()) / std_dev_vec.clone();
+            println!("{:?}", feature_col); 
+        }
+
+
+
+
+        Array2::zeros(self.data.dim())
+    }
+
+    /// Decode function for decoding One Hot Encoded Values
+    fn decode(&mut self, data: Array2<f64>) -> Array2<f64> {
+        Array2::zeros(self.data.dim())
     }
 
 }
--- crates/dendritic-preprocessing/tests/preprocessing_tests.rs
+++ crates/dendritic-preprocessing/tests/preprocessing_tests.rs
@@ -13,7 +13,19 @@ mod preprocessing_tests {
             [0.0], [0.0], [0.0], [1.0], [1.0], [1.0], [2.0], [2.0], [2.0]
         ]);
 
-        let one_hot = OneHotEncoding::new(&x).unwrap();
+        let encoded = arr2(&[
+            [1.0,0.0,0.0],
+            [1.0,0.0,0.0],
+            [1.0,0.0,0.0],
+            [0.0,1.0,0.0],
+            [0.0,1.0,0.0],
+            [0.0,1.0,0.0],
+            [0.0,0.0,1.0],
+            [0.0,0.0,1.0],
+            [0.0,0.0,1.0]
+        ]);
+
+        let mut one_hot = OneHotEncoding::new(&x).unwrap();
         let bad_on_hot = OneHotEncoding::new(
             &arr2(&[
                  [0.0, 0.0],
@@ -24,24 +36,49 @@ mod preprocessing_tests {
         assert_eq!(one_hot.num_classes(), 3); 
         assert_eq!(one_hot.num_samples(), 9); 
         assert_eq!(one_hot.data().dim(), x.dim());
-        assert_eq!(one_hot.encoded().dim(), (x.nrows(), 3));
 
         assert_eq!(
             bad_on_hot.unwrap_err().to_string(),
             "Input must be of size (N, 1)"
         );
+
+        assert_eq!(one_hot.encode(), encoded);
+
+        let decoded = one_hot.decode(encoded);
+        assert_eq!(decoded, x); 
  
     }
 
 
     #[test]
-    fn test_min_max_scalar() {
+    fn test_standard_scalar() {
+
+        let x = arr2(&[
+            [1.0,2.0,1.0],
+            [2.0,2.0,6.0],
+            [3.0,3.0,5.0],
+            [4.0,1.0,4.0],
+            [5.0,1.0,3.0],
+            [6.0,1.0,2.0],
+            [7.0,4.0,1.0]
+        ]);
+
+        let mut standard_scalar = StandardScalar::new(&x).unwrap();
+
+        assert_eq!(standard_scalar.data(), x); 
+        assert_eq!(standard_scalar.data().dim(), x.dim());
+        assert_eq!(standard_scalar.mean().len(), 3); 
+        assert_eq!(standard_scalar.stdev().len(), 3);
+
+        let data = standard_scalar.encode(); 
+
+
 
     }
 
 
     #[test]
-    fn test_standard_scalar() {
+    fn test_min_max_scalar() {
 
     }
 
